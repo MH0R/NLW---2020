c7b7d37bd8e5a71f59843a7f1d9a91962c95ac09
Thalles <thalles3br@gmail.com>
2020-10-17 00:12:34 -0300

    Segundo dia

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,46 @@
+// create a map
+const options = {
+    draggin: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+const map = L.map('mapid', options).setView([-23.5346694, -46.4636076], 15)
+
+//create and add titlelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+// create a icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68,],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+
+//create and add marker
+
+L
+    .marker([-23.5346694, -46.4636076], { icon })
+    .addTo(map)
+
+/* image galery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach((button) => { button.classList.remove("active") })
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    imageContainer = document.querySelector(".orphanage-details > img")
+
+    //Atualizar o container de imagem
+    imageContainer.src = image.src
+    //Adicionar a classe .active para este botao
+    button.classList.add('active')
+
+}
